37b544274f8ebeb697fefe598bfc59d313495159
arthurgehrke <arthurgehrke07@gmail.com>
2020-05-03 15:18:45 -0300

    chore: add Cards and Chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import Cards from './components/Cards/Cards'
+import Chart from './components/Chart/Chart'
+
+import styles from './App.module.css'
+
 class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hi</h1>
+        <Cards />
+        <Chart />
       </div>
     )
   }
